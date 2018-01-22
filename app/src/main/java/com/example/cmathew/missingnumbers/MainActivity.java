cfc78e7334e03bbcf8c53716ccad7289fc2cdc5e
cmathew <chris.w.mathew@gmail.com>
2018-01-22 13:07:09 -0800

    define basic layout
    name and title elements, wire up listeners

--- app/src/main/java/com/example/cmathew/missingnumbers/MainActivity.java
+++ app/src/main/java/com/example/cmathew/missingnumbers/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.cmathew.missingnumbers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button gapButton = findViewById(R.id.determine_gaps_button);
+
+        gapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText text = findViewById(R.id.sequence_entry);
+            }
+        });
     }
 }
