11b7750f082f9388c5fef3b0fbefa34c20905186
cmathew <chris.w.mathew@gmail.com>
2018-01-22 13:35:15 -0800

    Compute and display missing ranges based on input. Provide sample input.

--- app/src/main/java/com/example/cmathew/missingnumbers/MainActivity.java
+++ app/src/main/java/com/example/cmathew/missingnumbers/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.cmathew.missingnumbers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.StringBufferInputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +24,40 @@ public class MainActivity extends AppCompatActivity {
         gapButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText text = findViewById(R.id.sequence_entry);
+                List<String> missingRanges = new ArrayList<>();
+
+                int[] numbers = getNumberList();
+                int i = 0;
+                int j = 1;
+                while (i < numbers.length && j < numbers.length) {
+                    // detect missing numbers
+                    int difference = numbers[j] - numbers[i];
+                    if (difference == 2) {
+                        missingRanges.add(String.format("%d", numbers[i] + 1));
+                    } else if (difference > 2) {
+                        missingRanges.add(String.format("%d - %d", numbers[i] + 1, numbers[j] - 1));
+                    }
+
+                    i++;
+                    j++;
+                }
+
+                TextView display = findViewById(R.id.ranges_found_display);
+                String output = TextUtils.join(", ", missingRanges);
+                display.setText(output);
             }
         });
+    }
+
+    private int[] getNumberList() {
+        EditText text = findViewById(R.id.sequence_entry);
+        String input = text.getText().toString();
+        String[] numberStrings = input.split(",\\s*");
+        int[] numbers = new int[numberStrings.length];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = Integer.parseInt(numberStrings[i]);
+        }
+
+        return numbers;
     }
 }
